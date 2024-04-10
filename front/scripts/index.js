b8f82c22559ec9337af3f04f768069d66fcacd3a
lucasmzd <ilucasmzd@gmail.com>
2024-04-10 13:25:38 -0300

    HW Dia 2 Solucionado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -11,21 +11,11 @@ tempData.map ((pelicula) => {
     divPelicula.innerHTML = `<h3 class="divPelicula_nombre">${pelicula.title} (${pelicula.year})</h3>
                                 <p class="divPelicula_director"><strong>Director: </strong>${pelicula.director}</p>
                                 <p class="divPelicula_duracion"><strong>Duración: </strong>${pelicula.duration}</p>
-                                <p class="divPelicula_genero"><strong>Género: </strong>${pelicula.genre}</p>
+                                <p class="divPelicula_genero"><strong>Género: </strong>${pelicula.genre.join(', ')}</p>
                                 <p class="divPelicula_puntuacion"><strong>Puntuación: </strong>${pelicula.rate}</p>`;
     
     articuloPelicula.appendChild(divPelicula);
     seccionPelicula.appendChild(articuloPelicula);
 });
 
-// callBack = (data) => {
-//     const contenedor = document.getElementById("sectionPeliculas")
-//     data.forEach(element => {
-        
-//         contenedor.appendChild(..(element))
-
-//     });
-// }
-// $.get("https://students-api.up.railway.app/movies", (data)=>{
-//     callBack(data)
-// });
+$.get("https://students-api.up.railway.app/movies", (data)=> data.map(tempData)).fail(()=> alert("Error al cargar las películas"))
