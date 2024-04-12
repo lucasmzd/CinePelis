9023bb76c27277bae8f2eef775babd403a9bd9de
lucasmzd <ilucasmzd@gmail.com>
2024-04-12 12:32:18 -0300

    Cambios de Node

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,21 +1,31 @@
-const seccionPelicula = document.getElementById("sectionPeliculas");
+const renderFilms = require ("./renderFilms");
 
-tempData.map ((pelicula) => {
-    const articuloPelicula = document.createElement ("article");
-    const divPelicula = document.createElement ("div");
+$.get("https://students-api.up.railway.app/movies", (data) => {
+    renderFilms(data);
+}).fail(() => alert("Error al cargar las películas"));
 
-    articuloPelicula.classList.add ("articlePelicula");
-    divPelicula.classList.add ("divPelicula");
 
-    articuloPelicula.innerHTML = `<img src="${pelicula.poster}" alt="${pelicula.title}">`;
-    divPelicula.innerHTML = `<h3 class="divPelicula_nombre">${pelicula.title} (${pelicula.year})</h3>
-                                <p class="divPelicula_director"><strong>Director: </strong>${pelicula.director}</p>
-                                <p class="divPelicula_duracion"><strong>Duración: </strong>${pelicula.duration}</p>
-                                <p class="divPelicula_genero"><strong>Género: </strong>${pelicula.genre.join(', ')}</p>
-                                <p class="divPelicula_puntuacion"><strong>Puntuación: </strong>${pelicula.rate}</p>`;
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// const seccionPelicula = document.getElementById("sectionPeliculas");
+
+// const renderFilms = (pelicula) => {
+//     const articuloPelicula = document.createElement ("article");
+//     const divPelicula = document.createElement ("div");
+
+//     articuloPelicula.classList.add ("articlePelicula");
+//     divPelicula.classList.add ("divPelicula");
+
+//     articuloPelicula.innerHTML = `<img src="${pelicula.poster}" alt="${pelicula.title}">`;
+//     divPelicula.innerHTML = `<h3 class="divPelicula_nombre">${pelicula.title} (${pelicula.year})</h3>
+//                                 <p class="divPelicula_director"><strong>Director: </strong>${pelicula.director}</p>
+//                                 <p class="divPelicula_duracion"><strong>Duración: </strong>${pelicula.duration}</p>
+//                                 <p class="divPelicula_genero"><strong>Género: </strong>${pelicula.genre.join(', ')}</p>
+//                                 <p class="divPelicula_puntuacion"><strong>Puntuación: </strong>${pelicula.rate}</p>`;
     
-    articuloPelicula.appendChild(divPelicula);
-    seccionPelicula.appendChild(articuloPelicula);
-});
+//     articuloPelicula.appendChild(divPelicula);
+//     seccionPelicula.appendChild(articuloPelicula);
+// };
 
-$.get("https://students-api.up.railway.app/movies", (data)=> data.map(tempData)).fail(()=> alert("Error al cargar las películas"))
+// $.get("https://students-api.up.railway.app/movies", (data)=> data.map(tempData)).fail(()=> alert("Error al cargar las películas"))
