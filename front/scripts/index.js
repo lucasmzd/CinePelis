0cadc7ee9fa0025fff190bcc4e8b1387be120f73
lucasmzd <ilucasmzd@gmail.com>
2024-04-17 09:17:22 -0300

    Solucionado errores HW C5

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,31 +1,36 @@
 const renderFilms = require ("./renderFilms");
+const axios = require("axios");
 
-$.get("https://students-api.up.railway.app/movies", (data) => {
-    renderFilms(data);
-}).fail(() => alert("Error al cargar las películas"));
+const renderPeliculas = async () => {
+    try {
+        const result = await axios.get("https://students-api.up.railway.app/movies");
+        // result.data.forEach(renderFilms);
+        renderFilms(result.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+renderPeliculas();
+// const promise = axios.get('https://students-api.up.railway.app/movies')
+
+// promise
+//   .then((res) => {
+//     renderFilms(res.data);
+//   })
+//   .catch((err) => {
+//     alert(err.message);
+//   });
 
 
 
-// ---------------------------------------------------------------------------------------------------------------------------
 
-// const seccionPelicula = document.getElementById("sectionPeliculas");
 
-// const renderFilms = (pelicula) => {
-//     const articuloPelicula = document.createElement ("article");
-//     const divPelicula = document.createElement ("div");
 
-//     articuloPelicula.classList.add ("articlePelicula");
-//     divPelicula.classList.add ("divPelicula");
 
-//     articuloPelicula.innerHTML = `<img src="${pelicula.poster}" alt="${pelicula.title}">`;
-//     divPelicula.innerHTML = `<h3 class="divPelicula_nombre">${pelicula.title} (${pelicula.year})</h3>
-//                                 <p class="divPelicula_director"><strong>Director: </strong>${pelicula.director}</p>
-//                                 <p class="divPelicula_duracion"><strong>Duración: </strong>${pelicula.duration}</p>
-//                                 <p class="divPelicula_genero"><strong>Género: </strong>${pelicula.genre.join(', ')}</p>
-//                                 <p class="divPelicula_puntuacion"><strong>Puntuación: </strong>${pelicula.rate}</p>`;
-    
-//     articuloPelicula.appendChild(divPelicula);
-//     seccionPelicula.appendChild(articuloPelicula);
-// };
 
-// $.get("https://students-api.up.railway.app/movies", (data)=> data.map(tempData)).fail(()=> alert("Error al cargar las películas"))
+// $.get("https://students-api.up.railway.app/movies", (data) => {
+//     renderFilms(data);
+// }).fail(() => alert("Error al cargar las películas"));
+
+
