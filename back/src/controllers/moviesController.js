60abd00025d70c863ac477cd86bbfc502bae2ca0
lucasmzd <ilucasmzd@gmail.com>
2024-04-24 14:32:36 -0300

    crearNuevaPelicula Fix

--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -1,9 +1,20 @@
+const moviesService = require("../services/moviesService")
 const {getMovies} = require("../services/moviesService")
-
 
 const moviesControl = async (req, res) =>{
     const movies = await getMovies()
     res.status(200).send(movies)
 }
 
-module.exports = moviesControl
+const crearNuevaPelicula = async (req, res) => {
+    try {
+        const {title, year, director, duration, genre, rate, poster}=req.body
+        const respuestaCrearNuevaPelicula = await moviesService.createMovie({title, year, director, duration, genre, rate, poster})
+        res.status(201).json({ message: "Película creada correctamente", respuestaCrearNuevaPelicula });
+    } catch (error) {
+        res.status(500).json({ error: "No se pudo crear la película" });
+    }
+}
+
+module.exports = {moviesControl, crearNuevaPelicula}
+
