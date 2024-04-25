e6f15747c9754de51e74e06881445c7e5093676b
lucasmzd <ilucasmzd@gmail.com>
2024-04-24 23:33:48 -0300

    Extra credit middleware

--- back/src/routes/moviesRouter.js
+++ back/src/routes/moviesRouter.js
@@ -1,9 +1,10 @@
 const {Router} = require ("express")
 const {moviesControl, crearNuevaPelicula} = require("../controllers/moviesController")
+const dataCheck = require ("../middlewares/dataCheck")
 
 const moviesRouter = Router()
 
 moviesRouter.get("/movies", moviesControl);
-moviesRouter.post("/movies", crearNuevaPelicula);
+moviesRouter.post("/movies", dataCheck, crearNuevaPelicula);
 
 module.exports = moviesRouter
